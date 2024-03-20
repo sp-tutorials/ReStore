b5fa891ac72864920ee497c7ab71600731ff2aac
Patrulescu-Ronald-Sandrino <prie2985@scs.ubbcluj.ro>
2024-03-20 21:29:37 +0200

    3.36. Using Material UI Theme

--- client/src/app/layout/App.tsx
+++ client/src/app/layout/App.tsx
@@ -1,38 +1,25 @@
-import { useEffect, useState } from "react";
-import { Product } from "../models/product.ts";
 import Catalog from "../../features/catalog/Catalog.tsx";
 import Header from "./Header.tsx";
-import { Container, CssBaseline } from "@mui/material";
+import { Container, createTheme, CssBaseline, ThemeProvider } from "@mui/material";
+import { useState } from "react";
 
 function App() {
-  const [products, setProducts] = useState<Product[]>([]);
-
-  useEffect(() => {
-    fetch('http://localhost:5000/api/products')
-      .then(response => response.json())
-      .then(data => setProducts(data))
-  }, [])
-
-  function addProduct() {
-    setProducts(prevState => [...prevState,
-      {
-        id: prevState.length + 1,
-        name: 'product' + (prevState.length + 1),
-        price: (prevState.length * 100) + 100,
-        brand: 'some brand',
-        description: 'description',
-        pictureUrl: 'http://picsum.photos/200',
-      }])
-  }
+  const [darkMode, setDarkMode] = useState(false);
+  const paletteType = darkMode ? 'dark' : 'light';
+  const theme = createTheme({
+    palette: {
+      mode: paletteType
+    }
+  })
 
   return (
-    <>
+    <ThemeProvider theme={theme}>
       <CssBaseline />
       <Header />
       <Container>
-        <Catalog products={products} addProduct={addProduct} />
+        <Catalog />
       </Container>
-    </>
+    </ThemeProvider>
   );
 }
 
--- client/src/features/catalog/Catalog.tsx
+++ client/src/features/catalog/Catalog.tsx
@@ -1,17 +1,19 @@
 import { Product } from "../../app/models/product.ts";
-import { Button } from "@mui/material";
 import ProductList from "./ProductList.tsx";
+import { useEffect, useState } from "react";
 
-interface Props {
-  products: Product[];
-  addProduct: () => void;
-}
+export default function Catalog() {
+  const [products, setProducts] = useState<Product[]>([]);
 
-export default function Catalog({products, addProduct}: Props) {
+  useEffect(() => {
+    fetch('http://localhost:5000/api/products')
+      .then(response => response.json())
+      .then(data => setProducts(data))
+  }, [])
+
   return (
     <>
       <ProductList products={products} />
-      <Button variant='contained' onClick={addProduct}>Add product</Button>
     </>
   )
 }
