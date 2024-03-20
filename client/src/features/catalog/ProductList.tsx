600f3a05e2373ce8437472ce7d429e42d1907a11
Patrulescu-Ronald-Sandrino <prie2985@scs.ubbcluj.ro>
2024-03-20 19:55:58 +0200

    3.34. Adding cards for our individual products

--- client/src/features/catalog/ProductList.tsx
+++ client/src/features/catalog/ProductList.tsx
@@ -1,4 +1,4 @@
-import { List } from "@mui/material";
+import { Grid } from "@mui/material";
 import { Product } from "../../app/models/product.ts";
 import ProductCard from "./ProductCard.tsx";
 
@@ -8,10 +8,12 @@ interface Props {
 
 export default function ProductList({products}: Props) {
   return (
-    <List>
+    <Grid container spacing={4}>
       {products.map(product => (
-        <ProductCard key={product.id} product={product} />
+        <Grid item xs={4} key={product.id}>
+          <ProductCard product={product} />
+        </Grid>
       ))}
-    </List>
+    </Grid>
   )
 }
