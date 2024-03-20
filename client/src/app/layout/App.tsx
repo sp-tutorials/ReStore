4b55759ed28c3323cb3b8954bebd15a8a5029160
Patrulescu-Ronald-Sandrino <prie2985@scs.ubbcluj.ro>
2024-03-20 19:46:32 +0200

    3.33. Adding an AppBar

--- client/src/app/layout/App.tsx
+++ client/src/app/layout/App.tsx
@@ -1,7 +1,8 @@
 import { useEffect, useState } from "react";
 import { Product } from "../models/product.ts";
 import Catalog from "../../features/catalog/Catalog.tsx";
-import { Typography } from "@mui/material";
+import Header from "./Header.tsx";
+import { Container, CssBaseline } from "@mui/material";
 
 function App() {
   const [products, setProducts] = useState<Product[]>([]);
@@ -25,10 +26,13 @@ function App() {
   }
 
   return (
-    <div>
-      <Typography variant='h1'>Re-Store</Typography>
-      <Catalog products={products} addProduct={addProduct} />
-    </div>
+    <>
+      <CssBaseline />
+      <Header />
+      <Container>
+        <Catalog products={products} addProduct={addProduct} />
+      </Container>
+    </>
   );
 }
 
