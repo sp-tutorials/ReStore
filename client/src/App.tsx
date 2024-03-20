e7f42766bf3401048271a32e0a962dec180161a5
Patrulescu-Ronald-Sandrino <prie2985@scs.ubbcluj.ro>
2024-03-20 18:21:13 +0200

    3.23. Using react hooks - useState

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,16 @@
-const products = [
-  {name: 'product1', price: 100.00},
-  {name: 'product2', price: 200.00},
-  {name: 'product2', price: 200.00},
-]
+import { useState } from "react";
 
 function App() {
+  const [products, setProducts] = useState([
+    {name: 'product1', price: 100.00},
+    {name: 'product2', price: 200.00},
+  ]);
+
+  function addProduct() {
+    setProducts(prevState => [...prevState,
+      {name: 'product' + (prevState.length + 1), price: (prevState.length * 100) + 100}])
+  }
+
   return (
     <div>
       <h1>Re-Store</h1>
@@ -13,6 +19,7 @@ function App() {
           <li key={index}>{item.name} - {item.price}</li>
         ))}
       </ul>
+      <button onClick={addProduct}>Add product</button>
     </div>
   );
 }
