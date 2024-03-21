f0c486900ef99bc2773fa86fc72cef3dea0ddee7
Patrulescu-Ronald-Sandrino <prie2985@scs.ubbcluj.ro>
2024-03-21 17:58:41 +0200

    5.52. Adding toast notifications

--- client/src/app/layout/App.tsx
+++ client/src/app/layout/App.tsx
@@ -2,6 +2,8 @@ import Header from "./Header.tsx";
 import { Container, createTheme, CssBaseline, ThemeProvider } from "@mui/material";
 import { useState } from "react";
 import { Outlet } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
+import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
   const [darkMode, setDarkMode] = useState(false);
@@ -21,6 +23,7 @@ function App() {
 
   return (
     <ThemeProvider theme={theme}>
+      <ToastContainer position="bottom-right" hideProgressBar theme="colored" />
       <CssBaseline />
       <Header darkMode={darkMode} handleThemeChange={handleThemeChange} />
       <Container>
