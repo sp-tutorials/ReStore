0178d72798e63c4f95ed5dda97102169ccaae1c3
Patrulescu-Ronald-Sandrino <prie2985@scs.ubbcluj.ro>
2024-03-21 17:03:51 +0200

    5.49. Centralising the axios requests

--- client/src/features/catalog/Catalog.tsx
+++ client/src/features/catalog/Catalog.tsx
@@ -1,14 +1,13 @@
 import { Product } from "../../app/models/product.ts";
 import ProductList from "./ProductList.tsx";
 import { useEffect, useState } from "react";
+import agent from "../../app/api/agent.ts";
 
 export default function Catalog() {
   const [products, setProducts] = useState<Product[]>([]);
 
   useEffect(() => {
-    fetch('http://localhost:5000/api/products')
-      .then(response => response.json())
-      .then(data => setProducts(data))
+    agent.Catalog.list().then(products => setProducts(products))
   }, [])
 
   return (
