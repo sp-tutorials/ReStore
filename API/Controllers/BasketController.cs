95209206cb55dfa1418a2d55685e3a67cf8a8e3f
Patrulescu-Ronald-Sandrino <prie2985@scs.ubbcluj.ro>
2024-03-22 16:02:52 +0200

    6.68. Removing an item from the basket

--- API/Controllers/BasketController.cs
+++ API/Controllers/BasketController.cs
@@ -62,10 +62,17 @@ public class BasketController : BaseApiController
     [HttpDelete]
     public async Task<ActionResult> RemoveBasketItem(int productId, int quantity)
     {
-        // get basket
-        // remove the item or reduce quantity
-        // save changes
-        return Ok();
+        var basket = await RetrieveBasket();
+
+        if (basket == null) return NotFound();
+
+        basket.RemoveItem(productId, quantity);
+
+        var result = await _context.SaveChangesAsync() > 0;
+
+        if (result) return Ok();
+
+        return BadRequest(new ProblemDetails { Title = "Problem removing item from the basket" });
     }
 
     private async Task<Basket> RetrieveBasket()
