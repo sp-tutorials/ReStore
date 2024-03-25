4992ff28ce2776856cbfa28ec4b8e5e3aa128816
Patrulescu-Ronald-Sandrino <prie2985@scs.ubbcluj.ro>
2024-03-25 19:51:21 +0200

    7.86. Redux actions

--- client/src/features/contact/ContactPage.tsx
+++ client/src/features/contact/ContactPage.tsx
@@ -1,8 +1,9 @@
-import { Typography } from "@mui/material";
-import { useSelector } from "react-redux";
-import { CounterState } from "./counterReducer.ts";
+import { Button, ButtonGroup, Typography } from "@mui/material";
+import { useDispatch, useSelector } from "react-redux";
+import { CounterState, DECREMENT_COUNTER, INCREMENT_COUNTER } from "./counterReducer.ts";
 
 export default function ContactPage() {
+  const dispatch = useDispatch();
   const {data, title} = useSelector((state: CounterState) => state);
 
   return (
@@ -13,6 +14,10 @@ export default function ContactPage() {
       <Typography variant="h5">
         The data is: {data}
       </Typography>
+      <ButtonGroup>
+        <Button onClick={() => dispatch({type: DECREMENT_COUNTER})} variant='contained' color='error'>Decrement</Button>
+        <Button onClick={() => dispatch({type: INCREMENT_COUNTER})} variant='contained' color='primary'>Increment</Button>
+      </ButtonGroup>
     </>
   )
 }
