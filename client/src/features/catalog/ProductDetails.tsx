5f8036163044048c6aa4b3e3adf821ad4a936282
Patrulescu-Ronald-Sandrino <prie2985@scs.ubbcluj.ro>
2024-03-25 18:59:43 +0200

    6.81. Updating the product detail component to add items to cart part 2

--- client/src/features/catalog/ProductDetails.tsx
+++ client/src/features/catalog/ProductDetails.tsx
@@ -10,7 +10,7 @@ import {
   Typography
 } from "@mui/material";
 import { useParams } from "react-router-dom";
-import { useEffect, useState } from "react";
+import { ChangeEvent, useEffect, useState } from "react";
 import { Product } from "../../app/models/product.ts";
 import agent from "../../app/api/agent.ts";
 import NotFound from "../../app/errors/NotFound.tsx";
@@ -19,7 +19,7 @@ import { useStoreContext } from "../../app/context/StoreContext.tsx";
 import { LoadingButton } from "@mui/lab";
 
 export default function ProductDetails() {
-  const {basket} = useStoreContext();
+  const {basket, setBasket, removeItem} = useStoreContext();
   const {id} = useParams<{ id: string }>();
   const [product, setProduct] = useState<Product | null>(null);
   const [loading, setLoading] = useState(true);
@@ -34,6 +34,30 @@ export default function ProductDetails() {
       .catch(error => console.log(error))
       .finally(() => setLoading(false))
   }, [id, item]);
+
+  function handleInputChange(event: ChangeEvent<HTMLInputElement>) {
+    if (parseInt(event.currentTarget.value) >= 0) {
+      setQuantity(parseInt(event.currentTarget.value));
+    }
+  }
+
+  function handleUpdateCart() {
+    if (!product) return;
+    setSubmitting(true);
+    if (!item || quantity > item.quantity) {
+      const updatedQuantity = item ? quantity - item.quantity : quantity;
+      agent.Basket.addItem(product.id!, updatedQuantity)
+        .then(basket => setBasket(basket))
+        .catch(error => console.log(error))
+        .finally(() => setSubmitting(false))
+    } else {
+      const updatedQuantity = item.quantity - quantity;
+      agent.Basket.removeItem(product.id!, updatedQuantity)
+        .then(() => removeItem(product.id!, updatedQuantity))
+        .catch(error => console.log(error))
+        .finally(() => setSubmitting(false))
+    }
+  }
 
   if (loading) return <LoadingComponent message='Loading product...' />
 
@@ -73,6 +97,7 @@ export default function ProductDetails() {
         <Grid container spacing={2}>
           <Grid item xs={6}>
             <TextField
+              onChange={handleInputChange}
               variant='outlined'
               type='number'
               label='Quantity in Cart'
@@ -82,6 +107,9 @@ export default function ProductDetails() {
           </Grid>
           <Grid item xs={6}>
             <LoadingButton
+              disabled={item?.quantity === quantity || !item && quantity === 0}
+              loading={submitting}
+              onClick={handleUpdateCart}
               sx={{height: '55px'}}
               color='primary'
               size='large'
