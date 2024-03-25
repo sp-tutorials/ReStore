4927f719d64727ba643c7730ee85a7c801219ca5
Patrulescu-Ronald-Sandrino <prie2985@scs.ubbcluj.ro>
2024-03-25 13:25:35 +0200

    6.71. Creating a basket component

--- client/src/app/layout/Header.tsx
+++ client/src/app/layout/Header.tsx
@@ -1,5 +1,5 @@
 import { AppBar, Badge, Box, IconButton, List, ListItem, Switch, Toolbar, Typography } from "@mui/material";
-import { NavLink } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
 import { ShoppingCart } from "@mui/icons-material";
 
 const midLinks = [
@@ -59,7 +59,7 @@ export default function Header({darkMode, handleThemeChange}: Props) {
         </List>
 
         <Box display='flex' alignItems='center'>
-          <IconButton size='large' edge='start' color='inherit' sx={{mr: 2}}>
+          <IconButton component={Link} to='/basket' size='large' edge='start' color='inherit' sx={{mr: 2}}>
             <Badge badgeContent='4' color="secondary">
               <ShoppingCart />
             </Badge>
--- client/src/app/router/Routes.tsx
+++ client/src/app/router/Routes.tsx
@@ -7,6 +7,7 @@ import AboutPage from "../../features/about/AboutPage.tsx";
 import ContactPage from "../../features/contact/ContactPage.tsx";
 import ServerError from "../errors/ServerError.tsx";
 import NotFound from "../errors/NotFound.tsx";
+import BasketPage from "../../features/basket/BasketPage.tsx";
 
 export const router = createBrowserRouter([
   {
@@ -20,6 +21,7 @@ export const router = createBrowserRouter([
       {path: 'contact', element: <ContactPage />},
       {path: 'server-error', element: <ServerError />},
       {path: 'not-found', element: <NotFound />},
+      {path: 'basket', element: <BasketPage />},
       {path: '*', element: <Navigate replace to='/not-found' />},
     ]
   }
