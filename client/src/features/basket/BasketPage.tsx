53aa98fe281764f9719d9655d6a4ecced80d1fd1
Patrulescu-Ronald-Sandrino <prie2985@scs.ubbcluj.ro>
2024-03-26 14:07:15 +0200

    7.95. Challenge - Solution

--- client/src/features/basket/BasketPage.tsx
+++ client/src/features/basket/BasketPage.tsx
@@ -50,15 +50,19 @@ export default function BasketPage() {
                 <TableCell align="right">${(item.price / 100).toFixed(2)}</TableCell>
                 <TableCell align="center">
                   <LoadingButton
-                    loading={status.includes('pendingRemoveItem' + item.productId)}
-                    onClick={() => dispatch(removeBasketItemAsync({productId: item.productId}))}
+                    loading={status === 'pendingRemoveItem' + item.productId + 'rem'}
+                    onClick={() => dispatch(removeBasketItemAsync({
+                      productId: item.productId,
+                      quantity: 1,
+                      name: 'rem'
+                    }))}
                     color='error'
                   >
                     <Remove />
                   </LoadingButton>
                   {item.quantity}
                   <LoadingButton
-                    loading={status.includes('pendingAddItem' + item.productId)}
+                    loading={status === 'pendingAddItem' + item.productId}
                     onClick={() => dispatch(addBasketItemAsync({productId: item.productId}))}
                     color='secondary'
                   >
@@ -68,10 +72,11 @@ export default function BasketPage() {
                 <TableCell align="right">${((item.price / 100) * item.quantity).toFixed(2)}</TableCell>
                 <TableCell align="right">
                   <LoadingButton
-                    loading={status.includes('pendingRemoveItem' + item.productId)}
+                    loading={status === 'pendingRemoveItem' + item.productId + 'del'}
                     onClick={() => dispatch(removeBasketItemAsync({
                       productId: item.productId,
-                      quantity: item.quantity
+                      quantity: item.quantity,
+                      name: 'del'
                     }))}
                     color='error'
                   >
