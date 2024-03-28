a8b4739b681beb0d634754ba1bd7d0c006042c72
Patrulescu-Ronald-Sandrino <prie2985@scs.ubbcluj.ro>
2024-03-28 22:30:55 +0200

    8.116. Adding filtering to the client

--- client/src/features/catalog/Catalog.tsx
+++ client/src/features/catalog/Catalog.tsx
@@ -5,15 +5,13 @@ import { useAppDispatch, useAppSelector } from "../../app/store/configureStore.t
 import { fetchFilters, fetchProductsAsync, productSelectors, setProductParams } from "./catalogSlice.ts";
 import {
   Box,
-  Checkbox,
-  FormControlLabel,
-  FormGroup,
   Grid, Pagination,
   Paper,
   Typography
 } from "@mui/material";
 import ProductSearch from "./ProductSearch.tsx";
 import RadioButtonGroup from "../../app/components/RadioButtonGroup.tsx";
+import CheckboxButtons from "../../app/components/CheckboxButtons.tsx";
 
 const sortOptions = [
   {value: 'name', label: 'Alphabetical'},
@@ -51,19 +49,19 @@ export default function Catalog() {
         </Paper>
 
         <Paper sx={{mb: 2, p: 2}}>
-          <FormGroup>
-            {brands.map(brand => (
-              <FormControlLabel control={<Checkbox />} label={brand} key={brand} />
-            ))}
-          </FormGroup>
+          <CheckboxButtons
+            items={brands}
+            checked={productParams.brands}
+            onChange={(items: string[]) => dispatch(setProductParams({brands: items}))}
+          />
         </Paper>
 
         <Paper sx={{mb: 2, p: 2}}>
-          <FormGroup>
-            {types.map(type => (
-              <FormControlLabel control={<Checkbox />} label={type} key={type} />
-            ))}
-          </FormGroup>
+          <CheckboxButtons
+            items={types}
+            checked={productParams.types}
+            onChange={(items: string[]) => dispatch(setProductParams({types: items}))}
+          />
         </Paper>
       </Grid>
       <Grid item xs={9}>
