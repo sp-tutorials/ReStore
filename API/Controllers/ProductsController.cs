593a07ecf149b724b6b9928ca566e83b925e1474
Patrulescu-Ronald-Sandrino <prie2985@scs.ubbcluj.ro>
2024-03-28 13:39:21 +0200

    8.107. Creating a class that extends our List into a PagedList

--- API/Controllers/ProductsController.cs
+++ API/Controllers/ProductsController.cs
@@ -1,8 +1,9 @@
+using System.Text.Json;
 using API.Data;
 using API.Entities;
 using API.Extensions;
+using API.RequestHelpers;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers;
 
@@ -16,16 +17,20 @@ public class ProductsController : BaseApiController
     }
 
     [HttpGet]
-    public async Task<ActionResult<List<Product>>> GetProducts(string orderBy, string searchTerm, string brands,
-        string types)
+    public async Task<ActionResult<PagedList<Product>>> GetProducts(ProductParams productParams)
     {
         var query = _context.Products
-            .Sort(orderBy)
-            .Search(searchTerm)
-            .Filter(brands, types)
+            .Sort(productParams.OrderBy)
+            .Search(productParams.SearchTerms)
+            .Filter(productParams.Brands, productParams.Types)
             .AsQueryable();
 
-        return await query.ToListAsync();
+        var products = await PagedList<Product>.ToPagedList(query,
+            productParams.PageNumber, productParams.PageSize);
+
+        Response.Headers.Add("Pagination", JsonSerializer.Serialize(products.MetaData));
+
+        return products;
     }
 
     [HttpGet("{id}")]
