274da2ff5e971a970fdacbb7a95b1f04b9164ee2
Patrulescu-Ronald-Sandrino <prie2985@scs.ubbcluj.ro>
2024-03-31 16:27:24 +0300

    9.134. Using controlled component

--- client/src/features/account/Login.tsx
+++ client/src/features/account/Login.tsx
@@ -1,4 +1,3 @@
-import * as React from 'react';
 import Avatar from '@mui/material/Avatar';
 import Button from '@mui/material/Button';
 import TextField from '@mui/material/TextField';
@@ -9,16 +8,24 @@ import Typography from '@mui/material/Typography';
 import Container from '@mui/material/Container';
 import { Paper } from "@mui/material";
 import { Link } from "react-router-dom";
+import { useState } from "react";
+import agent from "../../app/api/agent.ts";
 
 export default function Login() {
-  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+  const [values, setValues] = useState({
+    username: '',
+    password: ''
+  });
+
+  const handleSubmit = (event: any) => {
     event.preventDefault();
-    const data = new FormData(event.currentTarget);
-    console.log({
-      email: data.get('email'),
-      password: data.get('password'),
-    });
+    agent.Account.login(values);
   };
+
+  function handleInputChange(event: any) {
+    const {name, value} = event.target;
+    setValues({...values, [name]: value});
+  }
 
   return (
     <Container component={Paper} maxWidth="sm"
@@ -32,23 +39,21 @@ export default function Login() {
       <Box component="form" onSubmit={handleSubmit} noValidate sx={{mt: 1}}>
         <TextField
           margin="normal"
-          required
           fullWidth
-          id="email"
-          label="Email Address"
-          name="email"
-          autoComplete="email"
+          label="Username"
+          name="username"
           autoFocus
+          onChange={handleInputChange}
+          value={values.username}
         />
         <TextField
           margin="normal"
-          required
           fullWidth
           name="password"
           label="Password"
           type="password"
-          id="password"
-          autoComplete="current-password"
+          onChange={handleInputChange}
+          value={values.password}
         />
         <Button type="submit" fullWidth variant="contained" sx={{mt: 3, mb: 2}}>
           Sign In
