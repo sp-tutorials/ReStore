85761fe08d647df8f8515880d6c7eba99bb32a42
Patrulescu-Ronald-Sandrino <prie2985@scs.ubbcluj.ro>
2024-04-06 18:01:55 +0300

    9.138. Creating a dropdown menu

--- client/src/app/layout/Header.tsx
+++ client/src/app/layout/Header.tsx
@@ -2,6 +2,7 @@ import { AppBar, Badge, Box, IconButton, List, ListItem, Switch, Toolbar, Typogr
 import { Link, NavLink } from "react-router-dom";
 import { ShoppingCart } from "@mui/icons-material";
 import { useAppSelector } from "../store/configureStore.ts";
+import SignedInMenu from "./SignedInMenu.tsx";
 
 const midLinks = [
   {title: 'catalog', path: '/catalog'},
@@ -33,6 +34,7 @@ interface Props {
 
 export default function Header({darkMode, handleThemeChange}: Props) {
   const {basket} = useAppSelector(state => state.basket);
+  const {user} = useAppSelector(state => state.account);
   const itemCount = basket?.items.reduce((sum, item) => sum + item.quantity, 0);
 
   return (
@@ -68,18 +70,22 @@ export default function Header({darkMode, handleThemeChange}: Props) {
               <ShoppingCart />
             </Badge>
           </IconButton>
-          <List sx={{display: 'flex'}}>
-            {rightLinks.map(({title, path}) => (
-              <ListItem
-                component={NavLink}
-                to={path}
-                key={path}
-                sx={navStyles}
-              >
-                {title.toUpperCase()}
-              </ListItem>
-            ))}
-          </List>
+          {user ? (
+            <SignedInMenu />
+          ) : (
+            <List sx={{display: 'flex'}}>
+              {rightLinks.map(({title, path}) => (
+                <ListItem
+                  component={NavLink}
+                  to={path}
+                  key={path}
+                  sx={navStyles}
+                >
+                  {title.toUpperCase()}
+                </ListItem>
+              ))}
+            </List>
+          )}
         </Box>
 
       </Toolbar>
--- client/src/features/account/accountSlice.ts
+++ client/src/features/account/accountSlice.ts
@@ -2,6 +2,7 @@ import { FieldValues } from "react-hook-form";
 import { User } from "../../app/models/user.ts";
 import { createAsyncThunk, createSlice, isAnyOf } from "@reduxjs/toolkit";
 import agent from "../../app/api/agent.ts";
+import { router } from "../../app/router/Routes.tsx";
 
 interface AccountState {
   user: User | null;
@@ -40,7 +41,13 @@ export const fetchCurrentUser = createAsyncThunk<User>(
 export const accountSlice = createSlice(({
   name: 'account',
   initialState,
-  reducers: {},
+  reducers: {
+    signOut: (state) => {
+      state.user = null;
+      localStorage.removeItem('user');
+      router.navigate('/');
+    }
+  },
   extraReducers: (builder => {
     builder.addMatcher(isAnyOf(signInUser.fulfilled, fetchCurrentUser.fulfilled), (state, action) => {
       state.user = action.payload;
@@ -50,3 +57,5 @@ export const accountSlice = createSlice(({
     })
   })
 }))
+
+export const {signOut} = accountSlice.actions;
