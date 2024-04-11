d379d2d2b824eeabef051059c8c6629903786dda
Patrulescu-Ronald-Sandrino <prie2985@scs.ubbcluj.ro>
2024-04-11 23:39:28 +0300

    9.146. Adding a private route component

--- client/src/app/router/Routes.tsx
+++ client/src/app/router/Routes.tsx
@@ -11,12 +11,18 @@ import BasketPage from "../../features/basket/BasketPage.tsx";
 import CheckoutPage from "../../features/checkout/CheckoutPage.tsx";
 import Login from "../../features/account/Login.tsx";
 import Register from "../../features/account/Register.tsx";
+import RequireAuth from "./RequireAuth.tsx";
 
 export const router = createBrowserRouter([
   {
     path: '/',
     element: <App />,
     children: [
+      {
+        element: <RequireAuth />, children: [
+          {path: 'checkout', element: <CheckoutPage />},
+        ]
+      },
       {path: '', element: <HomePage />},
       {path: 'catalog', element: <Catalog />},
       {path: 'catalog/:id', element: <ProductDetails />},
@@ -25,7 +31,6 @@ export const router = createBrowserRouter([
       {path: 'server-error', element: <ServerError />},
       {path: 'not-found', element: <NotFound />},
       {path: 'basket', element: <BasketPage />},
-      {path: 'checkout', element: <CheckoutPage />},
       {path: 'login', element: <Login />},
       {path: 'register', element: <Register />},
       {path: '*', element: <Navigate replace to='/not-found' />},
