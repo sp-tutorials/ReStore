a438d3e696199f7e55dddb30e73bb05b60c31166
Patrulescu-Ronald-Sandrino <prie2985@scs.ubbcluj.ro>
2024-04-20 15:45:39 +0300

    13.209. Adding a product form

--- client/src/features/admin/Inventory.tsx
+++ client/src/features/admin/Inventory.tsx
@@ -16,16 +16,33 @@ import useProducts from "../../app/hooks/useProducts.tsx";
 import { AppPagination } from "../../app/components/AppPagination.tsx";
 import { useAppDispatch } from "../../app/store/configureStore.ts";
 import { setPageNumber } from "../catalog/catalogSlice.ts";
+import { useState } from "react";
+import ProductForm from "./ProductForm.tsx";
+import { Product } from "../../app/models/product.ts";
 
 export default function Inventory() {
   const {products, metaData} = useProducts();
   const dispatch = useAppDispatch();
+  const [editMode, setEditMode] = useState(false);
+  const [selectedProduct, setSelectedProduct] = useState<Product | undefined>(undefined);
+
+  function handleSelectProduct(product: Product) {
+    setSelectedProduct(product);
+    setEditMode(true);
+  }
+
+  function cancelEdit() {
+    if (selectedProduct) setSelectedProduct(undefined);
+    setEditMode(false);
+  }
+
+  if (editMode) return <ProductForm product={selectedProduct} cancelEdit={cancelEdit} />
 
   return (
     <>
       <Box display='flex' justifyContent='space-between'>
         <Typography sx={{p: 2}} variant='h4'>Inventory</Typography>
-        <Button sx={{m: 2}} size='large' variant='contained'>Create</Button>
+        <Button onClick={() => setEditMode(true)} sx={{m: 2}} size='large' variant='contained'>Create</Button>
       </Box>
       <TableContainer component={Paper}>
         <Table sx={{minWidth: 650}} aria-label="simple table">
@@ -60,7 +77,7 @@ export default function Inventory() {
                 <TableCell align="center">{product.brand}</TableCell>
                 <TableCell align="center">{product.quantityInStock}</TableCell>
                 <TableCell align="right">
-                  <Button startIcon={<Edit />} />
+                  <Button onClick={() => handleSelectProduct(product)} startIcon={<Edit />} />
                   <Button startIcon={<Delete />} color='error' />
                 </TableCell>
               </TableRow>
