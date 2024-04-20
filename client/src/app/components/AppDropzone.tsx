8ef57f02c3338711f2f7b7f6d96d9afaf0df91f2
Patrulescu-Ronald-Sandrino <prie2985@scs.ubbcluj.ro>
2024-04-20 18:18:07 +0300

    13.213. Styling the drop zone

--- client/src/app/components/AppDropzone.tsx
+++ client/src/app/components/AppDropzone.tsx
@@ -1,12 +1,30 @@
 import { useCallback } from 'react'
 import { useDropzone } from 'react-dropzone'
 import { useController, UseControllerProps } from "react-hook-form";
+import { FormControl, FormHelperText, Typography } from "@mui/material";
+import { UploadFile } from "@mui/icons-material";
 
 interface Props extends UseControllerProps {
 }
 
 export default function AppDropzone(props: Props) {
   const {fieldState, field} = useController({...props, defaultValue: null});
+
+  const dzStyles = {
+    display: 'flex',
+    border: 'dashed 3px #eee',
+    borderColor: '#eee',
+    borderRadius: '5px',
+    paddingTop: '30px',
+    alignItems: 'center',
+    height: 200,
+    width: 500
+  }
+
+  const dzActive = {
+    borderColor: 'green'
+  }
+
   const onDrop = useCallback((acceptedFiles: any) => {
     acceptedFiles[0] = Object.assign(acceptedFiles[0], {preview: URL.createObjectURL(acceptedFiles[0])});
     field.onChange(acceptedFiles[0]);
@@ -15,12 +33,12 @@ export default function AppDropzone(props: Props) {
 
   return (
     <div {...getRootProps()}>
-      <input {...getInputProps()} />
-      {
-        isDragActive ?
-          <p>Drop the files here ...</p> :
-          <p>Drag 'n' drop some files here, or click to select files</p>
-      }
+      <FormControl style={isDragActive ? {...dzStyles, ...dzActive} : dzStyles}>
+        <input {...getInputProps()} />
+        <UploadFile sx={{fontSize: '100px'}} />
+        <Typography variant='h4'>Drop image here</Typography>
+        <FormHelperText>{fieldState.error?.message}</FormHelperText>
+      </FormControl>
     </div>
   )
 }
